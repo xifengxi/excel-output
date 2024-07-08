332cc920cce48630e80b74abb647159502ea03da
lishiyao <lishiyao@hortorgames.com>
2024-07-08 10:56:16 +0800

    fix

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o diffexecl main.go
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o diffexecl.exe main.go
+CGO_ENABLED=0 GOOS=darwin GOARCH=amd64 go build -o diffexcel main.go
+CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o diffexcel.exe main.go
